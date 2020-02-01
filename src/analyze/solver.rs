8bac186a9adf3db53caeae715556c5e5ed870e3c
Yechan Bae <yechan@gatech.edu>
2020-01-31 22:37:49 -0500

    Implement algorithm W1

--- src/analyze/solver.rs
+++ src/analyze/solver.rs
@@ -4,17 +4,68 @@ use super::{Constraint, ConstraintSet};
 
 type NodeSet = HashSet<usize>;
 
-/// Algorithm W1 from "Field-sensitive pointer analysis for C"
-pub struct SolverW1 {
+struct WorkList {
     /// node ID that should be handled
     work_list: VecDeque<usize>,
-    num_node: usize,
-    /// direct address set
-    sol: Vec<NodeSet>,
+    in_list: Vec<bool>,
+}
+
+impl WorkList {
+    fn new(num_node: usize) -> Self {
+        WorkList {
+            work_list: VecDeque::new(),
+            in_list: vec![false; num_node],
+        }
+    }
+
+    fn push(&mut self, target: usize) {
+        if self.in_list[target] == false {
+            self.in_list[target] = true;
+            self.work_list.push_back(target);
+        }
+    }
+
+    fn pop(&mut self) -> Option<usize> {
+        let result = self.work_list.pop_front();
+        if let Some(idx) = result {
+            self.in_list[idx] = false;
+        }
+        result
+    }
+}
+
+struct StateW1 {
+    work_list: WorkList,
     /// idx -> to
     from: Vec<NodeSet>,
     /// idx <- from
     to: Vec<NodeSet>,
+}
+
+impl StateW1 {
+    fn new(num_node: usize) -> Self {
+        StateW1 {
+            work_list: WorkList::new(num_node),
+            from: vec![NodeSet::new(); num_node],
+            to: vec![NodeSet::new(); num_node],
+        }
+    }
+
+    fn add_edge(&mut self, src: usize, dst: usize) {
+        self.from[src].insert(dst);
+        self.to[dst].insert(src);
+    }
+
+    fn contains_edge(&mut self, src: usize, dst: usize) -> bool {
+        self.from[src].contains(&dst)
+    }
+}
+
+/// Algorithm W1 from "Field-sensitive pointer analysis for C"
+pub struct SolverW1 {
+    state: StateW1,
+    /// direct address set
+    sol: Vec<NodeSet>,
     /// target >= *idx
     load: Vec<NodeSet>,
     /// *idx >= target
@@ -26,11 +77,6 @@ pub struct SolverW1 {
 impl SolverW1 {
     fn add_sol(&mut self, ptr: usize, loc: usize) {
         self.sol[ptr].insert(loc);
-    }
-
-    fn add_edge(&mut self, src: usize, dst: usize) {
-        self.from[src].insert(dst);
-        self.to[dst].insert(src);
     }
 
     fn add_load(&mut self, src: usize, dst: usize) {
@@ -45,18 +91,11 @@ impl SolverW1 {
         self.store_addr[dst].insert(src);
     }
 
-    fn contains_edge(&mut self, src: usize, dst: usize) -> bool {
-        self.from[src].contains(&dst)
-    }
-
     pub fn solve<T: ConstraintSet>(set: &T) -> Self {
         let num_node = set.num_locations();
         let mut solver = SolverW1 {
-            work_list: VecDeque::new(),
-            num_node,
             sol: vec![NodeSet::new(); num_node],
-            from: vec![NodeSet::new(); num_node],
-            to: vec![NodeSet::new(); num_node],
+            state: StateW1::new(num_node),
             load: vec![NodeSet::new(); num_node],
             store: vec![NodeSet::new(); num_node],
             store_addr: vec![NodeSet::new(); num_node],
@@ -66,14 +105,64 @@ impl SolverW1 {
             use Constraint::*;
             match constraint {
                 AddrOf(dst) => solver.add_sol(src, dst),
-                Copy(dst) => solver.add_edge(src, dst),
+                Copy(dst) => solver.state.add_edge(src, dst),
                 Load(dst) => solver.add_load(src, dst),
                 Store(dst) => solver.add_store(src, dst),
                 StoreAddr(dst) => solver.add_store_addr(src, dst),
             }
         }
 
-        todo!();
+        // initialize work list
+        for node in 0..num_node {
+            solver.state.work_list.push(node);
+        }
+
+        // process work list
+        while let Some(idx) = solver.state.work_list.pop() {
+            for &target in solver.load[idx].iter() {
+                for &k in solver.sol[idx].iter() {
+                    if !solver.state.contains_edge(k, target) {
+                        solver.state.add_edge(k, target);
+                        solver.state.work_list.push(k);
+                    }
+                }
+            }
+
+            for &target in solver.store[idx].iter() {
+                for &k in solver.sol[idx].iter() {
+                    if !solver.state.contains_edge(target, k) {
+                        solver.state.add_edge(target, k);
+                        solver.state.work_list.push(target);
+                    }
+                }
+            }
+
+            for &target in solver.store_addr[idx].iter() {
+                let mut delayed = Vec::new();
+                for &k in solver.sol[idx].iter() {
+                    if !solver.sol[k].contains(&target) {
+                        delayed.push((k, target));
+                    }
+                }
+
+                for &(k, target) in delayed.iter() {
+                    solver.sol[k].insert(target);
+                    solver.state.work_list.push(k);
+                }
+            }
+
+            for &target in solver.state.from[idx].iter() {
+                let union: HashSet<_> = solver.sol[target]
+                    .union(&solver.sol[idx])
+                    .map(|&id| id)
+                    .collect();
+
+                if union.len() > solver.sol[target].len() {
+                    solver.sol[target] = union;
+                    solver.state.work_list.push(target);
+                }
+            }
+        }
 
         solver
     }
