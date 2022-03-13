9a25419409dda277ba7fcf2c8570c3be157d9856
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-13 14:44:27 +0800

    Delete tests directory

--- tests/send_sync/okay_imm.rs
+++
@@ -1,9 +0,0 @@
-/*!
-```rudra-test
-test_type = "normal"
-expected_analyzers = []
-```
-!*/
-
-struct Atom<P>(P);
-unsafe impl<P: Ord + Sync> Sync for Atom<P> {}
--- tests/send_sync/wild_send.rs
+++
@@ -1,9 +0,0 @@
-/*!
-```rudra-test
-test_type = "normal"
-expected_analyzers = ["SendSyncVariance"]
-```
-!*/
-
-struct Atom<P>(P);
-unsafe impl<P: Ord> Send for Atom<P> {}
